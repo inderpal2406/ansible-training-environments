ed838c4a1a36ba809efdf19f6e3d04585e4a241b
Inderpal Singh <inderpal2406@gmail.com>
2022-07-24 18:47:41 +0530

    Add ssh-keys folder to .gitignore file.

--- terraform-files/ec2-instances.tf
+++ terraform-files/ec2-instances.tf
@@ -1,3 +1,14 @@
 # Define EC2 instances.
 
-#resource "aws_instance" "bastion-server" {}
+# Bastion server.
+/*
+resource "aws_instance" "bastion-server" {
+    ami = "ami-068257025f72f470d"
+    instance_type = "t2.micro"
+    tags = {
+        Name = "bastion-server"
+        Terraform = "True"
+        Owner = "Vikram Singh"
+    }
+}
+*/
