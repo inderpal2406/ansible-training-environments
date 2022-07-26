9bd93ebdaff15bf2e73d8a58e56d4c1b35c9a804
Inderpal Singh <inderpal2406@gmail.com>
2022-07-26 13:54:41 +0530

    Update arguments for bastion-server in ec2-instances.tf file.

--- terraform-files/ec2-instances.tf
+++ terraform-files/ec2-instances.tf
@@ -2,25 +2,39 @@
 
 # Before creating EC2 instance, have the security group and key-pair ready beforehand.
 
-# Bastion server.
-/*
-resource "aws_instance" "bastion-server" {
-    ami = "ami-068257025f72f470d"
-    instance_type = "t2.micro"
-    key_name = aws_key_pair.bastion-server-key.key_name
-    subnet_id = aws_subnet.main-vpc-pub-subnet.id
-    associate_public_ip_address = true
-    vpc_security_group_ids = [aws_security_group.allow-public-ssh.id]
-    private_ip = ""
-    tenancy = "shared"
-    metadata_options {
-      http_endpoint = "enabled"
-      https_tokens = "required" # Enables IMDSv2 service only.
-    }
-    tags = {
-        Name = "bastion-server"
-        Terraform = "True"
-        Owner = "Vikram Singh"
-    }
+# Define private IP addresses of hosts as local values.
+
+locals {
+  bastion-server-pvt-ip = "10.0.0.4"
 }
-*/
+
+# Bastion server.
+
+resource "aws_instance" "bastion-server" {
+  ami                         = data.aws_ami.ubuntu.id
+  instance_type               = "t2.micro"
+  key_name                    = aws_key_pair.bastion-server-key.key_name
+  subnet_id                   = aws_subnet.main-vpc-pub-subnet.id
+  associate_public_ip_address = true
+  vpc_security_group_ids      = [aws_security_group.allow-public-ssh.id]
+  private_ip                  = local.bastion-server-pvt-ip
+  tenancy                     = "default" # shared.
+  metadata_options {
+    http_endpoint = "enabled"
+    http_tokens   = "required" # Enables IMDSv2 service only.
+  }
+  # By default private IP based hostname is setup always with a corresponding pvt DNS entry.
+  #private_dns_name_options {}
+  tags = {
+    Name      = "bastion-server"
+    Terraform = "True"
+    Owner     = "Vikram Singh"
+  }
+  user_data = <<EOF
+    #!/usr/bin/env bash
+    machine_name="bastion-server"
+    sudo hostnamectl set-hostname $machine_name
+    sudo echo "${local.bastion-server-pvt-ip} $machine_name" >> /etc/hosts 
+    EOF
+}
+
