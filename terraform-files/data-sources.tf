652c74a848c22ffb2c9703ecab19e872d8c02e01
Inderpal Singh <inderpal2406@gmail.com>
2022-07-26 13:55:11 +0530

    Add data-sources.tf file.

---
+++ terraform-files/data-sources.tf
@@ -0,0 +1,12 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"] # Canonical
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-20220609"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
