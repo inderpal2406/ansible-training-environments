0e422b7880576d63e64e334287d6bae5e61ef907
Inderpal Singh <inderpal2406@gmail.com>
2022-07-27 17:13:43 +0530

    Add template provider in main.tf which is commented later due to no usage.

--- terraform-files/main.tf
+++ terraform-files/main.tf
@@ -4,5 +4,16 @@ terraform {
       source  = "hashicorp/aws"
       version = "4.23.0"
     }
+    # The template provider is defined here.
+    # Below format of user_data requires template provider which has been archived by Hashicorp.
+    # Hashicorp now suggests to use templatefile() or cloudinit provider instead.
+    #user_data = data.template_file.bastion-server-init.rendered
+    /*
+    template = {
+      source  = "hashicorp/template"
+      version = "2.2.0"
+    }
+    */
+    # As it was downloaded due to init command, I manually deleted it from .terraform folder and commented it above.
   }
 }
