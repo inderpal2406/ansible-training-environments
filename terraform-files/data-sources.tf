6b1bbdf040154dc77fb3054483ba732497883e39
Inderpal Singh <inderpal2406@gmail.com>
2022-07-27 17:17:35 +0530

    Add data source using template provider which was commented later due to no usage.

--- terraform-files/data-sources.tf
+++ terraform-files/data-sources.tf
@@ -10,3 +10,18 @@ data "aws_ami" "ubuntu" {
     values = ["hvm"]
   }
 }
+# The template provider is used here to create a data source to be used in user_data in ec2-instances.tf file.
+# Below format of user_data requires template provider which has been archived by Hashicorp.
+# Hashicorp now suggests to use templatefile() or cloudinit provider instead.
+#user_data = data.template_file.bastion-server-init.rendered
+/*
+data "template_file" "bastion-server-init" {
+  template = file(".\\template-files\\bastion-server-init.sh.tpl")
+  vars = {
+    bastion_server_pvt_ip   = var.bastion-server-pvt-ip
+    bastion_server_hostname = var.bastion-server-hostname
+    ansible_server_pvt_ip   = var.ansible-server-pvt-ip
+    ansible_server_hostname = var.ansible-server-hostname
+  }
+}
+*/
