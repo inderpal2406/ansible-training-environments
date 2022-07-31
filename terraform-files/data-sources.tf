9025f4078a816085d5b230e6497e1216d6b89f81
Inderpal Singh <inderpal2406@gmail.com>
2022-07-31 20:51:17 +0530

    Add data sources for redhat & centos ami id.

--- terraform-files/data-sources.tf
+++ terraform-files/data-sources.tf
@@ -10,6 +10,7 @@ data "aws_ami" "ubuntu" {
     values = ["hvm"]
   }
 }
+
 # The template provider is used here to create a data source to be used in user_data in ec2-instances.tf file.
 # Below format of user_data requires template provider which has been archived by Hashicorp.
 # Hashicorp now suggests to use templatefile() or cloudinit provider instead.
@@ -25,3 +26,33 @@ data "template_file" "bastion-server-init" {
   }
 }
 */
+
+# RedHat Linux AMI - ami-05c8ca4485f8b138a for ansible-server but not used.
+
+data "aws_ami" "redhat" {
+  most_recent = true
+  owners      = ["309956199498"] # RedHat
+  filter {
+    name   = "name"
+    values = ["RHEL-8.6.0_HVM-20220503-x86_64-2-Hourly2-GP2"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+# CentOS Linux AMI for ansible-server but couldn't be used as it needs to be subscribed.
+
+data "aws_ami" "centos" {
+  most_recent = true
+  owners      = ["679593333241"]
+  filter {
+    name   = "name"
+    values = ["CentOS-8-ec2-8.2.2004-20200923-1.x86_64-471d022d-974f-4f9c-8e39-b00d9b583833-ami-03b6a1d995f5a5146.4"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
