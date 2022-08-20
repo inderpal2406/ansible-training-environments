ddfac80158c5199635068fbd816436f7252ab431
Inderpal Singh <inderpal2406@gmail.com>
2022-08-20 13:54:26 +0530

    Add files to define and configure aws provider in project-2.

---
+++ project-2/terraform/providers.tf
@@ -0,0 +1,4 @@
+provider "aws" {
+  profile = var.aws_profile
+  region  = "ap-south-1"
+}
---
+++ project-2/terraform/vars.tf
@@ -0,0 +1,5 @@
+variable "aws_profile" {
+  type        = string
+  description = "AWS programmatic credentials to be used from AWS profile."
+  sensitive   = true
+}
