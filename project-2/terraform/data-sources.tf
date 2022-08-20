287af556c83ad71a1375fdafea4c594abfd1cfce
Inderpal Singh <inderpal2406@gmail.com>
2022-08-20 20:00:22 +0530

    Add data sources to get ami ids of ubuntu & redhat in project-2.

---
+++ project-2/terraform/data-sources.tf
@@ -0,0 +1,29 @@
+# Data source to get ubuntu AMI ID (ami-068257025f72f470d) in ap-south-1 region.
+
+data "aws_ami" "ubuntu-ami-id" {
+  most_recent = true
+  owners      = ["099720109477"] # Canonical
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-20220609"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+# Data source to get redhat AMI ID (ami-05c8ca4485f8b138a) in ap-south-1 region.
+
+data "aws_ami" "redhat-ami-id" {
+  most_recent = true
+  owners      = ["309956199498"] # Redhat
+  filter {
+    name   = "name"
+    values = ["RHEL-8.6.0_HVM-20220503-x86_64-2-Hourly2-GP2"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
